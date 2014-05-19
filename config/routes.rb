d9b675dd679cd3494e970430d36a4018cd34faa4
Prelang Builder <builder@prelang.com>
2014-05-19 22:07:58 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 KnowledgeMs::Application.routes.draw do
+  get "landings/index"
   resources :topics
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
